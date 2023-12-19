33e80f02f100ab0cb4c3329c0b32aed03f92a7de
Andrey Shestopal <andreyshestopal@Andreys-MacBook-Pro.local>
2023-12-18 19:07:23 -0800

    minor change

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
 	images: {
 		domains: ["wkmzwtndexinmhbrjkzb.supabase.co"],
 	},
+	typescript: {
+		// Disable type checking
+		ignoreBuildErrors: true,
+	},
 };
 
 module.exports = nextConfig;
